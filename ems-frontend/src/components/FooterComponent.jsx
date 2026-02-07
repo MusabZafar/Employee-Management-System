0e91f38df024b666f05dd19bb6fb4538bd2f4b06
Musab Zafar <129366236+MusabZafar@users.noreply.github.com>
2026-02-07 19:54:30 +0500

    Update footer text in FooterComponent

--- ems-frontend/src/components/FooterComponent.jsx
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ const FooterComponent = () => {
   return (
     <div>
         <footer className='footer'>
-            <span>All rights reserved 2023 by javaguides</span>
+            <span>Have A Good Day</span>
         </footer>
 
     </div>
